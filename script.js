1cb5c1958ee8dc6e3acd1d476c50e24aaeb6d6ef
Paulo Henrique <paulinhoph.ph67@gmail.com>
2017-05-23 23:24:58 -0300

    java

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 var myImage = document.querySelector('img');
-
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/camila.jpg') {
-      myImage.setAttribute ('src','imagens/paulo.jpg');
-    } else {
+    if(mySrc === 'imagens/paulo.jpg') {
       myImage.setAttribute ('src','imagens/camila.jpg');
+    } else {
+      myImage.setAttribute ('src','imagens/paulo.jpg');
     }
 }
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
